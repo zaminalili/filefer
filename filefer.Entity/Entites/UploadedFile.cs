71afd33b827ff932617ef10507c91e2e6cf5c930
Zamin Alili <zaminalili29@gmail.com>
2023-03-04 09:18:20 +0400

    Add entity

---
+++ filefer.Entity/Entites/UploadedFile.cs
@@ -0,0 +1,11 @@
+﻿namespace filefer.Entity.Entites
+{
+    public class UploadedFile
+    {
+        public Guid Id { get; set; }
+        public string FileName { get; set; }
+        public string FileType { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+
+    }
+}
