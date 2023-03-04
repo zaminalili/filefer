271b79b7092b7b09359eed1c2790e19695ec6b19
Zamin Alili <zaminalili29@gmail.com>
2023-03-04 09:28:14 +0400

    Add database context

---
+++ filefer.Data/Context/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using filefer.Entity.Entites;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace filefer.Data.Context
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options) { }
+
+        DbSet<UploadedFile> UploadedFiles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
