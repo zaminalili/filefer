a346cb826e1cfcf751211f3ef6614b088231dc29
Zamin Alili <zaminalili29@gmail.com>
2023-03-04 10:29:17 +0400

    Add DataLayerExtensions
    Added Extension for configuration settings in data layer

---
+++ filefer.Data/Extensions/DataLayerExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace filefer.Data.Extensions
+{
+    public static class DataLayerExtensions
+    {
+        public static IServiceCollection LoadDataLayerExtensions(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            return services;
+        }
+    }
+}
