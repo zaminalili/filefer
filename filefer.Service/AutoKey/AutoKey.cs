57b13f28d8bc504627168685916588db1e846f26
Zamin Alili <zaminalili29@gmail.com>
2023-03-05 11:41:19 +0400

    Add key generate service
    Created a key generation service named "AutoKey"

---
+++ filefer.Service/AutoKey/AutoKey.cs
@@ -0,0 +1,29 @@
+﻿namespace filefer.Service.AutoKey
+{
+    public class AutoKey
+	{
+        Random random = new Random();
+
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        string newString = "";
+
+        public string CreateKey(int charLength, int intLength)
+        {  
+
+            for (int i = 0; i < charLength; i++)
+                newString += chars[random.Next(0, chars.Length)];
+
+            return newString + Convert.ToString(random.Next(Convert.ToInt32(Math.Pow(10, intLength - 1)), Convert.ToInt32(Math.Pow(10, intLength))));
+        }
+
+        public string CreateKey(int charLength)
+        {
+
+
+            for (int i = 0; i < charLength; i++)
+                newString += chars[random.Next(0, chars.Length)];
+
+            return newString;
+        }
+    }
+}
