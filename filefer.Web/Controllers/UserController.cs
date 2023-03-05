3c168ee56f1e6443f871bd658685c8e9c3aca0e2
Zamin Alili <zaminalili29@gmail.com>
2023-03-05 11:15:04 +0400

    Add User Controller

---
+++ filefer.Web/Controllers/UserController.cs
@@ -0,0 +1,15 @@
+﻿using filefer.Entity.Entites;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace filefer.Web.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
